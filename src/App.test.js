90ef911c9ee2a694a00ac42903fe809261094a52
Rick Umali <rickumali@gmail.com>
2023-07-08 22:32:50 -0400

    Adds test and updates test name

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Row and Col entry fields', () => {
   render(<App />);
   const rowEntry = screen.getByText(/Enter Row/i);
   expect(rowEntry).toBeInTheDocument();
+  const colEntry = screen.getByText(/Enter Col/i);
+  expect(colEntry).toBeInTheDocument(); 
 });
