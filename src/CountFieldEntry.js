c99fed6201707b3dcb87827e8c1b283a3edf1781
Rick Umali <rickumali@gmail.com>
2023-07-09 21:40:46 -0400

    Puts Input Field inside of Label

--- src/CountFieldEntry.js
+++ src/CountFieldEntry.js
@@ -1,8 +1,9 @@
-function CountFieldEntry({name}) {
+function CountFieldEntry({ name }) {
     return (
-        <div id={name+"_count"}>
-            <label htmlFor={name}>{name} Count: </label>
-            <input type="text" name={name} required size="5" pattern="[0-9]*" />
+        <div id={name + "_count"}>
+            <label htmlFor={name}>{name} Count:
+                <input type="text" name={name} required size="5" pattern="[0-9]*" />
+            </label>
             <span className="validity"></span>
         </div>
     );
