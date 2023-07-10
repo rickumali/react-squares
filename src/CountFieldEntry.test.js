e92c5db39b6a01eea5d854c93734a45c86ac587e
Rick Umali <rickumali@gmail.com>
2023-07-09 21:41:40 -0400

    Fixes component test to properly use Promise

--- src/CountFieldEntry.test.js
+++ src/CountFieldEntry.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import CountFieldEntry from './CountFieldEntry';
 
-test('renders Row and Col entry fields', () => {
+test('renders CountEntryField', () => {
   render(<CountFieldEntry name="Random"/>);
-  const randomEntry = screen.getByLabelText(/Random Count/i);
-  expect(randomEntry).toBeInTheDocument(); 
+  const randomEntry = screen.findByLabelText(/Random Count/i);
+  randomEntry.then((r) => {
+    r.toBeInTheDocument();
+  })
 });
