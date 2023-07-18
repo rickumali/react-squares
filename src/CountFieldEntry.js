7645cd3be9038e2c71343d64312dc7d54aadf5fb
Rick Umali <rickumali@gmail.com>
2023-07-17 21:32:17 -0400

    Adds onChange event handler
    NOTE: This is directly from Step 5 from 'thinking in React' tutorial at:
    
    https://react.dev/learn/thinking-in-react#step-5-add-inverse-data-flow

--- src/CountFieldEntry.js
+++ src/CountFieldEntry.js
@@ -1,8 +1,9 @@
-function CountFieldEntry({ name, value }) {
+function CountFieldEntry({ name, value, onValueChange }) {
     return (
         <div id={name + "_count"}>
             <label htmlFor={name}>{name} Count:
-                <input type="text" value={value} name={name} required size="5" pattern="[0-9]*" />
+                <input type="text" value={value} name={name} required size="5" pattern="[0-9]*" 
+                    onChange={(e) => onValueChange(e.target.value)}/>
             </label>
             <span className="validity"></span>
         </div>
