b61d622eaff2324c62df816d374df05757e94134
Rick Umali <rickumali@gmail.com>
2023-07-17 21:12:53 -0400

    Adds useState to App
    This follows Step 4 of "thinking in React":
    
    https://react.dev/learn/thinking-in-react#step-4-identify-where-your-state-should-live

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import Board from './Board';
 import CountFieldEntry from './CountFieldEntry';
 
 function App() {
+  const [rowCount, setRowCount] = useState(5);
+  const [columnCount, setColumnCount] = useState(6);
+
   return (
     <div className="App">
-      <CountFieldEntry name="Row"/>
-      <CountFieldEntry name="Column"/>
-      <Board rows={3} columns={3} />
+      <CountFieldEntry name="Row" />
+      <CountFieldEntry name="Column" />
+      <Board rows={rowCount} columns={columnCount} />
     </div>
   );
 }
