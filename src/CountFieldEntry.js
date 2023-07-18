8dd0f5c952d405e632ccb71359fea635655fb6a2
Rick Umali <rickumali@gmail.com>
2023-07-17 21:21:13 -0400

    Adds 'value' field to CountFieldEntry component
    The test was modified only to display the rendered component. I still
    have to learn how to do proper testing with testing-library.

--- src/CountFieldEntry.js
+++ src/CountFieldEntry.js
@@ -1,8 +1,8 @@
-function CountFieldEntry({ name }) {
+function CountFieldEntry({ name, value }) {
     return (
         <div id={name + "_count"}>
             <label htmlFor={name}>{name} Count:
-                <input type="text" name={name} required size="5" pattern="[0-9]*" />
+                <input type="text" value={value} name={name} required size="5" pattern="[0-9]*" />
             </label>
             <span className="validity"></span>
         </div>
--- src/CountFieldEntry.test.js
+++ src/CountFieldEntry.test.js
@@ -2,8 +2,9 @@ import { render, screen } from '@testing-library/react';
 import CountFieldEntry from './CountFieldEntry';
 
 test('renders CountEntryField', () => {
-  render(<CountFieldEntry name="Random"/>);
+  render(<CountFieldEntry name="Random" value="3" />);
   const randomEntry = screen.findByLabelText(/Random Count/i);
+  screen.debug();
   randomEntry.then((r) => {
     r.toBeInTheDocument();
   })
