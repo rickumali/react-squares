378dde1c39b943b8aa2972e5c29c8324e3f373e3
Rick Umali <rickumali@gmail.com>
2023-07-18 22:38:57 -0400

    Adds first story
    This was hard to get going, but it makes sense! I had to read:
    
    https://storybook.js.org/docs/react/get-started/setup
    
    and:
    
    https://storybook.js.org/tutorials/intro-to-storybook/react/en/simple-component/

---
+++ src/CountFieldEntry.stories.js
@@ -0,0 +1,14 @@
+import CountFieldEntry from './CountFieldEntry';
+
+export default {
+  component: CountFieldEntry,
+  title: 'The first component',
+  tags: ['uno', 'dos'],
+};
+
+export const Default = {
+  args: {
+    name: 'Whooo',
+    value: 25
+  }
+};
